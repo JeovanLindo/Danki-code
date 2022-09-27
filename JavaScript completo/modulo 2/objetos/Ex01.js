f21d68d9647f280b21d8841f4a826a6702ebc64f
Jeovan Lindo <jeovanjrlindo@gmail.com>
2022-09-27 12:24:49 -0300

    Initial commit

---
+++ JavaScript completo/modulo 2/objetos/Ex01.js
@@ -0,0 +1,24 @@
+//Orientação a objetos
+
+
+//Modo simples de se criar um objeto
+
+var meunome = {
+    name:"jeovan",
+    idade:"19",
+    peso:"65kg"
+}
+
+console.log(meunome.name);
+console.log(meunome.idade);
+console.log(meunome.peso);
+
+//segundo modo, usando funçôes
+
+function pessoa(){
+    this.name = 'jeovan'
+    this.idade = '19'
+}
+
+
+
